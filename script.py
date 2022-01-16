3ba12af957304202f486df2550cc38bbd794f8f3
danielfgmb <df.gomezb@uniandes.edu.co>
2022-01-16 04:21:25 -0500

    update

--- script.py
+++ script.py
@@ -21,8 +21,9 @@ univ = "Uniandes"
 lenght = 2.8155 #m
 cte_lenght = 14*10**(-6) #m/m°C http://www.elab.tecnico.ulisboa.pt/wiki/index.php?title=World_Pendulum
 t_measured=18.97 #°C
-filename_v= "resultados_uniandes.csv"
-directory = "/DataTidesUniandes/" #optional
+directory = "/DataTidesUniandes/" 
+filename_v= directory+"resultados_uniandes.csv"
+#optional
 
 def openConn(puertoExp):
     conn=False
@@ -144,7 +145,7 @@ def temperatureCorrection(data):
 
 
 
-def saveObservationCSV(filename,data,datetime,backup_directory="/DataTidesUniandes/backup-data"):
+def saveObservationCSV(filename,data,datetime,backup_directory="backup-data"):
 
 
     if len(data)>0:

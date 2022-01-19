7d5c377d50f9a0571c6968e0b93dc6e122207f66
danielfgmb <df.gomezb@uniandes.edu.co>
2022-01-18 23:27:45 -0500

    Update script_test.py

--- script_test.py
+++ script_test.py
@@ -240,7 +240,7 @@ def execute():
 
 backup_directory="backup-data"
 backup_directory = "~/DataTidesUniandes/"+backup_directory
-os.mkdir(backup_directory)
+os.system("mkdir "+backup_directory)
 f = open(backup_directory+"/"+"data"+str(datetime)+".csv", 'a')
 writer = csv.writer(f)
 writer.writerow(['hey','jude'])
